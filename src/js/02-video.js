e3e43636a0316d221940b7afa3c1b346f5930e62
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-10 00:37:10 +0200

    added 2th task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_PLAYER = "videoplayer-current-time";
+const startPlayingTime = gettingTimeFromStorage(CURRENT_TIME_PLAYER);
+
+function gettingTimeFromStorage(time) {
+    try {
+        const timeOBJ = localStorage.getItem(time);
+        return timeOBJ === null ? undefined : JSON.parse(timeOBJ);
+    } catch (error) {
+        console.error("Get state error: ", error.message);
+    }
+}
+
+function savingCurrentTimePlaying(currentTime) {
+    // console.log(currentTime);
+    try {
+        const timeStorage = JSON.stringify(currentTime.seconds);
+        localStorage.setItem(CURRENT_TIME_PLAYER, timeStorage);
+    } catch (error) {
+        console.error("Set state error: ", error.message);
+    }
+}
+
+player.on('timeupdate', throttle(savingCurrentTimePlaying, 1000));
+
+player.setCurrentTime(startPlayingTime).then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
