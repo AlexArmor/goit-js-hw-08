509644f4256d6843b839f6be2e28ab6578955a93
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-09 21:36:55 +0200

    first task added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryGroup = document.querySelector('.gallery');
+
+function createGallaryMarkup() {
+    const markup = galleryItems.map(elem => {
+        return `<a class="gallery__item" href="${elem.original}">
+        <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+      </a>`
+    }).join('');
+
+    galleryGroup.innerHTML = markup;
+}
+
+createGallaryMarkup();
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
